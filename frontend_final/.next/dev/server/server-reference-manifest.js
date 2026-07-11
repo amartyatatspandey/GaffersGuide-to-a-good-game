fdb7247db30b95ca6122710ddedeb126014ee35a
Anshul Singh <anshulsingh1907@gmail.com>
2026-07-11 10:10:46 +0530

    chore: remove generated build artifacts from version control

--- frontend_final/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"utNouMb+84Ln7v9IHp51HfSSVOGDicIwn8KoNlHsD6c=\"\n}"
--- frontend_final/.next/dev/static/chunks/app_workspace_page_tsx_0qfneel._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0owuf1n._.js","static/chunks/node_modules_0tzhte4._.js"],
-    source: "dynamic"
-});
